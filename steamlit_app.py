43e825f99ee1ae90fa88af4d71dcb9e060baeeab
AddisonYing <116080591+addisonyingstudy@users.noreply.github.com>
2022-10-18 13:43:59 +0100

    Update steamlit_app.py
    new text

--- steamlit_app.py
+++ steamlit_app.py
@@ -1,4 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Health Dinner')
-streamlit.text('Omega 3 & Blueberray Oatmeal')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
