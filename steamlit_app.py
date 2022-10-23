0d5841fb250d23048de5bafa4e2d0252dcc2f078
AddisonYing <116080591+addisonyingstudy@users.noreply.github.com>
2022-10-23 21:25:41 +0100

    Update steamlit_app.py
    add my fruit

--- steamlit_app.py
+++ steamlit_app.py
@@ -3,12 +3,6 @@ import pandas
 import requests
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("THE FRUIT LOAD LIST CONTAINS")
-streamlit.dataframe(my_data_rows)
 
 streamlit.title('My Parents New Health Dinner')
 
@@ -39,3 +33,13 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_cho
 
 fruit_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruit_normalized)
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT * FROM fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.text("THE FRUIT LOAD LIST CONTAINS")
+streamlit.dataframe(my_data_rows)
+
+add_my_fruit = streamlit.text_input('What fruit would you like to add','jackfruit')
+streamlit.text('Thanks for adding'+add_my_fruit)
