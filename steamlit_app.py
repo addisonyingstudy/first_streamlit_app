dba898e72aba4e9d83181e8c8d42437e804ea0d0
AddisonYing <116080591+addisonyingstudy@users.noreply.github.com>
2022-10-23 18:55:34 +0100

    Update steamlit_app.py
    api json response

--- steamlit_app.py
+++ steamlit_app.py
@@ -22,5 +22,9 @@ fruit_show = my_fruit_list.loc[fruit_selected]
 # Display the table on the page.
 streamlit.dataframe(fruit_show)
 
+streamlit.header('Fruityvice Fruit Advice')
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+
